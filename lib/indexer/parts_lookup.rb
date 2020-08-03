94420597ae144b3136b9063021c65d253a1856b1
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-08-03 20:34:32 +0100

    Revert "Add HTML attachment content to search index"
    This reverts commit 4ad6366b862d9530eb2397ffd105519b51574e62.

--- lib/indexer/parts_lookup.rb
+++ lib/indexer/parts_lookup.rb
@@ -1,5 +1,4 @@
 module Indexer
-  # assumes it's run after AttachmentsLookup
   class PartsLookup
     def initialize
       @logger = Logging.logger[self]
@@ -12,10 +11,10 @@ module Indexer
     def prepare_parts(doc_hash)
       return doc_hash unless doc_hash["parts"].nil?
 
-      attachments = doc_hash.fetch("attachments", []).select { |a| can_be_a_part?(doc_hash, a) }
+      attachments = doc_hash.fetch("attachments", []).select { |a| a["attachment_type"] == "html" }
       return doc_hash if attachments.empty?
 
-      doc_hash.merge("parts" => attachments.map { |a| fetch_attachment_part(a) })
+      doc_hash.merge("parts" => attachments.map { |a| fetch_attachment_part(a, return_raw_body) })
     end
 
   private
@@ -24,25 +23,61 @@ module Indexer
       Indexer::find_content_id(doc_hash, @logger)
     end
 
-    def fetch_attachment_part(attachment)
+    def fetch_attachment_part(attachment, return_raw_body)
       part = fetch_from_publishing_api(attachment["url"])
+      body = part.dig("details", "body")
 
       {
         "slug" => attachment["url"].split("/").last,
         "title" => attachment["title"],
-        "body" => summarise(part.dig("details", "body")),
+        "body" => return_raw_body ? body : summarise(body),
       }
     end
 
-    def can_be_a_part?(doc_hash, attachment)
-      return false unless attachment["url"]
-      return false unless attachment["content"]
+    def summarise(html_body)
+      return unless html_body
 
-      # we don't index full part URLs, only slugs, so we need to
-      # ensure the full prefix matches
-      return false unless attachment["url"].start_with? doc_hash["link"]
+      Loofah.document(html_body)
+        .to_text(encode_special_chars: false).squish
+        .truncate(75, omission: "…", separator: " ")
+    end
 
-      true
+    def fetch_from_publishing_api(base_path)
+      content_id = find_content_id(base_path)
+
+      begin
+        GdsApi.with_retries(maximum_number_of_attempts: 5) do
+          Services.publishing_api.get_live_content(content_id)
+        end
+      rescue GdsApi::TimedOutException => e
+        @logger.error("Timeout fetching content item for #{content_id}")
+        GovukError.notify(e,
+                          extra: {
+                            error_message: "Timeout fetching content item",
+                            content_id: content_id,
+                          })
+        raise Indexer::PublishingApiError
+      rescue GdsApi::HTTPNotFound => e
+        # If the Content ID no longer exists in the Publishing API, there isn't really much
+        # we can do at this point. There doesn't seem to be any compelling reason to record
+        # this in Sentry as there is no bug to fix.
+        @logger.error("HTTP not found error fetching content item for #{content_id}: #{e.message}")
+        nil
+      rescue GdsApi::HTTPErrorResponse => e
+        @logger.error("HTTP error fetching content item for #{content_id}: #{e.message}")
+        # We capture all GdsApi HTTP exceptions here so that we can send them
+        # manually to Sentry. This allows us to control the message and parameters
+        # such that errors are grouped in a sane manner.
+        GovukError.notify(e,
+                          extra: {
+                            message: "HTTP error fetching content item",
+                            content_id: content_id,
+                            error_code: e.code,
+                            error_message: e.message,
+                            error_details: e.error_details,
+                          })
+        raise Indexer::PublishingApiError
+      end
     end
   end
 end
--- spec/integration/indexer/parts_lookup_spec.rb
+++ spec/integration/indexer/parts_lookup_spec.rb
@@ -4,14 +4,38 @@ require "gds_api/test_helpers/publishing_api"
 RSpec.describe "PartslookupDuringIndexingTest" do
   include GdsApi::TestHelpers::PublishingApi
 
-  before do
+  it "indexes document with parts unchanged" do
+    stub_publishing_api_has_lookups(
+      "/foo" => "document-content-id",
+    )
+
+    stub_publishing_api_has_expanded_links(content_id: "document-content-id", expanded_links: {})
+
+    post "/government_test/documents", {
+      "link" => "/foo",
+      "parts" => [{ "slug" => "foo", "title" => "bar", "body" => "baz" }],
+      "attachments" => [
+        { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
+        { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html" },
+        { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "html" },
+      ],
+    }.to_json
+
+    expect_document_is_in_rummager(
+      {
+        "link" => "/foo",
+        "parts" => [{ "slug" => "foo", "title" => "bar", "body" => "baz" }],
+      },
+      index: "government_test",
+    )
+  end
+
+  it "indexes documents with attachments from publishing api" do
     stub_publishing_api_has_lookups(
       "/foo" => "document-content-id",
       "/foo/attachment-1" => "attachment-content-id-1",
       "/foo/attachment-2" => "attachment-content-id-2",
       "/foo/attachment-3" => "attachment-content-id-3",
-      "/bar/attachment-4" => "attachment-content-id-4",
-      "/baz/attachment-5" => "attachment-content-id-5",
     )
 
     stub_publishing_api_has_expanded_links(content_id: "document-content-id", expanded_links: {})
@@ -19,47 +43,15 @@ RSpec.describe "PartslookupDuringIndexingTest" do
     stub_publishing_api_has_item({ content_id: "attachment-content-id-1", publication_state: "published", details: { body: "<strong>body 1</strong>" } })
     stub_publishing_api_has_item({ content_id: "attachment-content-id-2", publication_state: "published", details: { body: "<em>body 2</em>" } })
     stub_publishing_api_has_item({ content_id: "attachment-content-id-3", publication_state: "published", details: { body: "<p>body 3</p>" } })
-    stub_publishing_api_has_item({ content_id: "attachment-content-id-4", publication_state: "published", details: { body: "body 4" } })
-    stub_publishing_api_has_item({ content_id: "attachment-content-id-5", publication_state: "published", details: { body: "body 5" } })
-  end
 
-  it "indexes document with parts unchanged" do
-    post "/government_test/documents",
-         {
-           "link" => "/foo",
-           "parts" => [{ "slug" => "foo", "title" => "bar", "body" => "baz" }],
-           "attachments" => [
-             { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "html" },
-           ],
-         }.to_json
-
-    expect_document_is_in_rummager(
-      {
-        "link" => "/foo",
-        "parts" => [{ "slug" => "foo", "title" => "bar", "body" => "baz" }],
-        "attachments" => [
-          { "title" => "attachment 1", "content" => "body 1" },
-          { "title" => "attachment 2", "content" => "body 2" },
-          { "title" => "attachment 3", "content" => "body 3" },
-        ],
-        "indexable_content" => "body 1 body 2 body 3",
-      },
-      index: "government_test",
-    )
-  end
-
-  it "indexes documents with attachments from publishing api" do
-    post "/government_test/documents",
-         {
-           "link" => "/foo",
-           "attachments" => [
-             { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "html" },
-           ],
-         }.to_json
+    post "/government_test/documents", {
+      "link" => "/foo",
+      "attachments" => [
+        { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
+        { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html" },
+        { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "html" },
+      ],
+    }.to_json
 
     expect_document_is_in_rummager(
       {
@@ -69,27 +61,31 @@ RSpec.describe "PartslookupDuringIndexingTest" do
           { "slug" => "attachment-2", "title" => "attachment 2", "body" => "body 2" },
           { "slug" => "attachment-3", "title" => "attachment 3", "body" => "body 3" },
         ],
-        "attachments" => [
-          { "title" => "attachment 1", "content" => "body 1" },
-          { "title" => "attachment 2", "content" => "body 2" },
-          { "title" => "attachment 3", "content" => "body 3" },
-        ],
-        "indexable_content" => "body 1 body 2 body 3",
       },
       index: "government_test",
     )
   end
 
   it "ignores non-html attachments" do
-    post "/government_test/documents",
-         {
-           "link" => "/foo",
-           "attachments" => [
-             { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "file" },
-           ],
-         }.to_json
+    stub_publishing_api_has_lookups(
+      "/foo" => "document-content-id",
+      "/foo/attachment-1" => "attachment-content-id-1",
+      "/foo/attachment-2" => "attachment-content-id-2",
+    )
+
+    stub_publishing_api_has_expanded_links(content_id: "document-content-id", expanded_links: {})
+
+    stub_publishing_api_has_item({ content_id: "attachment-content-id-1", publication_state: "published", details: { body: "<strong>body 1</strong>" } })
+    stub_publishing_api_has_item({ content_id: "attachment-content-id-2", publication_state: "published", details: { body: "<em>body 2</em>" } })
+
+    post "/government_test/documents", {
+      "link" => "/foo",
+      "attachments" => [
+        { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
+        { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html" },
+        { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "file" },
+      ],
+    }.to_json
 
     expect_document_is_in_rummager(
       {
@@ -97,90 +93,6 @@ RSpec.describe "PartslookupDuringIndexingTest" do
         "parts" => [
           { "slug" => "attachment-1", "title" => "attachment 1", "body" => "body 1" },
           { "slug" => "attachment-2", "title" => "attachment 2", "body" => "body 2" },
-        ],
-        "attachments" => [
-          { "title" => "attachment 1", "content" => "body 1" },
-          { "title" => "attachment 2", "content" => "body 2" },
-          { "title" => "attachment 3" },
-        ],
-        "indexable_content" => "body 1 body 2",
-      },
-      index: "government_test",
-    )
-  end
-
-  it "ignores attachments with a locale given that isn't 'en'" do
-    post "/government_test/documents",
-         {
-           "link" => "/foo",
-           "attachments" => [
-             { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-2", "title" => "attachment 2", "attachment_type" => "html", "locale" => "en" },
-             { "url" => "/foo/attachment-3", "title" => "attachment 3", "attachment_type" => "file", "locale" => "cy" },
-           ],
-         }.to_json
-
-    expect_document_is_in_rummager(
-      {
-        "link" => "/foo",
-        "parts" => [
-          { "slug" => "attachment-1", "title" => "attachment 1", "body" => "body 1" },
-          { "slug" => "attachment-2", "title" => "attachment 2", "body" => "body 2" },
-        ],
-        "attachments" => [
-          { "title" => "attachment 1", "content" => "body 1" },
-          { "title" => "attachment 2", "content" => "body 2" },
-        ],
-        "indexable_content" => "body 1 body 2",
-      },
-      index: "government_test",
-    )
-  end
-
-  it "ignores attachments where the URL doesn't match the parent/slug format" do
-    post "/government_test/documents",
-         {
-           "link" => "/foo",
-           "attachments" => [
-             { "url" => "/foo/attachment-1", "title" => "attachment 1", "attachment_type" => "html" },
-             { "url" => "/bar/attachment-4", "title" => "attachment 4", "attachment_type" => "html" },
-             { "url" => "/baz/attachment-5", "title" => "attachment 5", "attachment_type" => "html" },
-           ],
-         }.to_json
-
-    expect_document_is_in_rummager(
-      {
-        "link" => "/foo",
-        "parts" => [
-          { "slug" => "attachment-1", "title" => "attachment 1", "body" => "body 1" },
-        ],
-        "attachments" => [
-          { "title" => "attachment 1", "content" => "body 1" },
-          { "title" => "attachment 4", "content" => "body 4" },
-          { "title" => "attachment 5", "content" => "body 5" },
-        ],
-        "indexable_content" => "body 1 body 4 body 5",
-      },
-      index: "government_test",
-    )
-  end
-
-  it "treats attachments of unspecified type or URL as not HTML" do
-    post "/government_test/documents",
-         {
-           "link" => "/foo",
-           "attachments" => [
-             { "title" => "attachment 1", "attachment_type" => "html" },
-             { "url" => "/foo/attachment-2", "title" => "attachment 2" },
-           ],
-         }.to_json
-
-    expect_document_is_in_rummager(
-      {
-        "link" => "/foo",
-        "parts" => nil,
-        "attachments" => [
-          { "title" => "attachment 2" },
         ],
       },
       index: "government_test",
