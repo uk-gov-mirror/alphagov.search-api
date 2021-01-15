8f0df7be4f2726165273dc4f34898b165001e88f
Kevin Dew <kevindew@me.com>
2021-01-15 15:34:34 +0000

    Set a redis config explicitly
    This is easier to understand than the previous iteration and may fix
    some problems.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,5 +2,5 @@ require "govuk_sidekiq/sidekiq_initializer"
 
 GovukSidekiq::SidekiqInitializer.setup_sidekiq(
   ENV.fetch("GOVUK_APP_NAME", "search-api"),
-  {},
+  { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379") },
 )
